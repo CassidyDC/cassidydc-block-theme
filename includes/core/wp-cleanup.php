ab0c1bc0837f92a7e29531a9e19b595a8ce16695
Jacob Cassidy <jacob@jacobcassidy.com>
2026-03-21 13:55:17 +0700

    Rename wp-cleanup files and directories

--- includes/core/wp-cleanup.php
+++ includes/core/wp-cleanup.php
@@ -9,9 +9,9 @@
 declare( strict_types = 1 );
 namespace CassidyDC\BlockTheme;
 
-require_once get_theme_file_path( 'includes/core/cleanup/blocks.php' );
-require_once get_theme_file_path( 'includes/core/cleanup/directories.php' );
-require_once get_theme_file_path( 'includes/core/cleanup/fonts.php' );
-require_once get_theme_file_path( 'includes/core/cleanup/openverse.php' );
-require_once get_theme_file_path( 'includes/core/cleanup/patterns.php' );
-require_once get_theme_file_path( 'includes/core/cleanup/styles.php' );
+require_once get_theme_file_path( 'includes/core/wp-cleanup/wp-blocks.php' );
+require_once get_theme_file_path( 'includes/core/wp-cleanup/wp-directories.php' );
+require_once get_theme_file_path( 'includes/core/wp-cleanup/wp-fonts.php' );
+require_once get_theme_file_path( 'includes/core/wp-cleanup/wp-openverse.php' );
+require_once get_theme_file_path( 'includes/core/wp-cleanup/wp-patterns.php' );
+require_once get_theme_file_path( 'includes/core/wp-cleanup/wp-styles.php' );
